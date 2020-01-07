d796b3ca276421889ac821fa97944efa0be0d9a8
Jim Kitchen <jim22k@gmail.com>
2020-01-07 09:41:52 -0600

    Initial boilerplate

---
+++ README.md
@@ -0,0 +1,4 @@
+# metagraph
+Python library for running graph algorithms on a variety of hardware backends.
+Data representing the graph will be automatically converted between available hardware options
+to find an efficient solution.
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='metagraph',
+    version='0.0.1',
+    description='Graph algorithm solver across multiple hardware backends',
+    author='Anaconda, Inc.',
+    packages=['metagraph'],
+)
