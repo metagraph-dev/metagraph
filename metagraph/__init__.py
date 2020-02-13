5c891cb75c38b7fba3c19e61936011f6008ed184
Stan Seibert <sseibert@anaconda.com>
2020-02-13 09:57:57 -0600

    create global registry singleton at top level

--- metagraph/__init__.py
+++ metagraph/__init__.py
@@ -11,3 +11,30 @@ from .core.plugin import (
     abstract_algorithm,
     concrete_algorithm,
 )
+
+_SPECIAL_ATTRS = ["resolver", "algo", "translate", "typeof"]
+
+
+def __getattr__(name):
+    """Lazy load the global resolver to avoid circular dependencies with plugins."""
+
+    if name in _SPECIAL_ATTRS:
+        from .core.resolver import Resolver
+
+        res = Resolver()
+        res.load_plugins_from_environment()
+        globals()["resolver"] = res
+        globals()["algo"] = res.algo
+        globals()["translate"] = res.translate
+        globals()["typeof"] = res.typeof
+
+        return globals()[name]
+    else:
+        raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
+
+
+def __dir__():
+    attrs = list(globals().keys())
+    if "resolver" not in attrs:
+        attrs += _SPECIAL_ATTRS
+    return attrs
