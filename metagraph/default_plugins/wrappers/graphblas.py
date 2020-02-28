67d573df98aebab1ce95ee1a986f83ccb95cfd76
Erik Welch <erik.n.welch@gmail.com>
2020-02-28 11:02:42 -0600

    Use numpy dtypes for our dtypes.
    Fixes #15.
    
    Specifically, use an actual dtype object and not the type.
    For example, `np.int64` and `np.longlong` both map to `dtype('int64')`.  We use the latter.
    When converting to dtype, we can handle every dtype-like thing that numpy does such as 'int64'.
    
    We still have a set of dtypes that metagraph nominally understands and supports,
    and code is written such that we could easily make our own dtype objects.
    
    This opens up the world of dtype-aware translations.
    To see where things may get painful, we include more dtypes than we strictly need right now.
    Conversely, there are also numpy dtypes that we don't yet have, so we'll see how conversions
    between numpy-aware libraries work out.
    
    Also, this makes numpy a hard dependency.  This seems reasonable to me.
    I have not yet updated code that was conditional on numpy being installed,
    because I'm afraid of merge conflicts.  We'll update this after merges.

--- metagraph/default_plugins/wrappers/graphblas.py
+++ metagraph/default_plugins/wrappers/graphblas.py
@@ -5,17 +5,17 @@ from .. import grblas
 
 if grblas is not None:
     dtype_grblas_to_mg = {
-        grblas.dtypes.BOOL: dtypes.BOOL,
-        grblas.dtypes.INT8: dtypes.INT8,
-        grblas.dtypes.INT16: dtypes.INT16,
-        grblas.dtypes.INT32: dtypes.INT32,
-        grblas.dtypes.INT64: dtypes.INT64,
-        grblas.dtypes.UINT8: dtypes.UINT8,
-        grblas.dtypes.UINT16: dtypes.UINT16,
-        grblas.dtypes.UINT32: dtypes.UINT32,
-        grblas.dtypes.UINT64: dtypes.UINT64,
-        grblas.dtypes.FP32: dtypes.FLOAT32,
-        grblas.dtypes.FP64: dtypes.FLOAT64,
+        grblas.dtypes.BOOL: dtypes.bool,
+        grblas.dtypes.INT8: dtypes.int8,
+        grblas.dtypes.INT16: dtypes.int16,
+        grblas.dtypes.INT32: dtypes.int32,
+        grblas.dtypes.INT64: dtypes.int64,
+        grblas.dtypes.UINT8: dtypes.uint8,
+        grblas.dtypes.UINT16: dtypes.uint16,
+        grblas.dtypes.UINT32: dtypes.uint32,
+        grblas.dtypes.UINT64: dtypes.uint64,
+        grblas.dtypes.FP32: dtypes.float32,
+        grblas.dtypes.FP64: dtypes.float64,
     }
     dtype_mg_to_grblas = {v: k for k, v in dtype_grblas_to_mg.items()}
 
