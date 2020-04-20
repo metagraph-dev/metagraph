ddb94abd418632d50cb0fcb4694086921df0e1e5
Paul Nguyen <paul.tqh.nguyen@gmail.com>
2020-04-20 10:54:53 -0700

    Support Tuple Return Types
    We can now in our abstract and concrete algorithms declare the return type to be a tuple.
    
    This required support to be added in:
    * Resolver._normalize_concrete_algorithm_signature, where concrete algorithm signature checking happens.
    * MultiVerify.assert_equals, which is used to compare values during testing.
    
    Misc. Notes:
    * The logic of the outer conditional (that determined which path to take based on the concrete return type) was flipped in the implementation of _check_concrete_algorithm_return_signature from that of _normalize_concrete_algorithm_signature for both readability and commonality of the 3 cases.
    
    This allows us to commit the test, concrete algorithm, and abstract algorithm declaration for Bellman-Ford in the NetworkX plugin. This commit includes these changes.

--- metagraph/plugins/networkx/algorithms.py
+++ metagraph/plugins/networkx/algorithms.py
@@ -58,23 +58,31 @@ if has_networkx:
             weights="non-negative",
         )
 
-    # @concrete_algorithm("traversal.bellman_ford")
-    # def nx_bellman_ford(graph: NetworkXGraph, source_node: int) -> Tuple[PythonNodes, PythonNodes]:
-    #     predecessors_map, distance_map = nx.bellman_ford_predecessor_and_distance(graph.value, source_node)
-    #     single_parent_map = {child: parents[0] if len(predecessors_map)>0 else source_node for child, parents in predecessors_map.items()}
-    #     return (
-    #         PythonNodes(
-    #             single_parent_map,
-    #             node_index=graph.node_index,
-    #             dtype="int",
-    #             weights="non-negative",
-    #         ),
-    #         PythonNodes(
-    #             distance_map,
-    #             node_index=graph.node_index,
-    #             dtype="int",
-    #             weights="non-negative",
-    #         ))
+    @concrete_algorithm("traversal.bellman_ford")
+    def nx_bellman_ford(
+        graph: NetworkXGraph, source_node: int
+    ) -> Tuple[PythonNodes, PythonNodes]:
+        predecessors_map, distance_map = nx.bellman_ford_predecessor_and_distance(
+            graph.value, source_node
+        )
+        single_parent_map = {
+            child: parents[0] if len(parents) > 0 else source_node
+            for child, parents in predecessors_map.items()
+        }
+        return (
+            PythonNodes(
+                single_parent_map,
+                node_index=graph.node_index,
+                dtype="int",
+                weights="non-negative",
+            ),
+            PythonNodes(
+                distance_map,
+                node_index=graph.node_index,
+                dtype="int",
+                weights="non-negative",
+            ),
+        )
 
     @concrete_algorithm("vertex_ranking.betweenness_centrality")
     def nx_betweenness_centrality(
