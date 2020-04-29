98185bac99e792b3bb75b5e1f7b0b799576f1ed4
Jim Kitchen <jim22k@gmail.com>
2020-04-29 17:39:52 -0500

    Improvements to unit tests

--- metagraph/tests/conftest.py
+++ metagraph/tests/conftest.py
@@ -4,8 +4,6 @@ from metagraph.core.resolver import Resolver
 
 @pytest.fixture(scope="session")
 def default_plugin_resolver():
-    from metagraph.plugins import find_plugins
-
     res = Resolver()
-    res.register(**find_plugins())
+    res.load_plugins_from_environment()
     return res
