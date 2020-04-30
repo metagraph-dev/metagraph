76620f0a84d70300ae1f41b152561661c468bfec
Jim Kitchen <jim22k@gmail.com>
2020-04-30 15:49:09 -0500

    Only allow abstract signatures to define default values

--- metagraph/plugins/networkx/algorithms.py
+++ metagraph/plugins/networkx/algorithms.py
@@ -12,10 +12,7 @@ if has_networkx:
 
     @concrete_algorithm("link_analysis.pagerank")
     def nx_pagerank(
-        graph: NetworkXGraph,
-        damping: float = 0.85,
-        maxiter: int = 50,
-        tolerance: float = 1e-05,
+        graph: NetworkXGraph, damping: float, maxiter: int, tolerance: float,
     ) -> PythonNodes:
         pagerank = nx.pagerank(
             graph.value, alpha=damping, max_iter=maxiter, tol=tolerance, weight=None
--- metagraph/tests/conftest.py
+++ metagraph/tests/conftest.py
@@ -2,8 +2,21 @@ import pytest
 from metagraph.core.resolver import Resolver
 
 
+def pytest_addoption(parser):
+    parser.addoption(
+        "--no-plugins",
+        action="store_true",
+        help="Exclude plugins when running algorithm tests.",
+    )
+
+
 @pytest.fixture(scope="session")
-def default_plugin_resolver():
+def default_plugin_resolver(request):
     res = Resolver()
-    res.load_plugins_from_environment()
+    if request.config.getoption("--no-plugins"):
+        from metagraph.plugins import find_plugins
+
+        res.register(**find_plugins())
+    else:
+        res.load_plugins_from_environment()
     return res
