902766011fda803b7052c442b421780bcf8275b9
Paul Nguyen <paul.tqh.nguyen@gmail.com>
2020-05-15 14:23:53 -0700

    Add Katz, Louvain, and Label Propagation for NetworkX plugin
    This commit adds abstract algorithms, concrete algorithms, and tests for the Katz Centrality, Louvain Community Detection, and Label Propagation algorithms.
    
    This was motivated by pedagogical material being developed to study airline data.

--- metagraph/algorithms/pagerank.py
+++
@@ -1,9 +0,0 @@
-from metagraph import abstract_algorithm
-from metagraph.types import Graph, Nodes
-
-
-@abstract_algorithm("link_analysis.pagerank")
-def pagerank(
-    graph: Graph, damping: float = 0.85, maxiter: int = 50, tolerance: float = 1e-05
-) -> Nodes:
-    pass  # pragma: no cover
