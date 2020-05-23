c9eb182553290e141a6cf82c77cf38f0ca407e37
Paul Nguyen <paul.tqh.nguyen@gmail.com>
2020-05-23 11:40:37 -0700

    Partial progress on calling exact concrete algorithms.
    This commit is intended to be a no-op refactoring (with few additional features).
    
    This commit includes changes intended to allow users to call exactl algorithms by name using the following two mechanisms:
    * resolver.algo.category.abstract_algo.<plugin_label>(*args, **kwargs)
    * resolver.plugin.<plugin_label>.algo.category.<abstract_algo>
    
    It's intended that resolver.plugin.<plugin_label>.<etc> offer the same content as resolver.<etc> but with only the content relevant to the plugin denoted by <plugin_label>.
    
    This is partial progress. Tests for the above functionality is forthcoming.
    
    Some of the notable changes include:
    * I renamed Resolver.algo -> Resolver.algos to have the attributes of a Resolver instance follow the same convention.
    ** It is unclear if we have a preference for the plural or singular to be used for the Namespace attributes of a Resolver instance. I chose the plural since (prior to these changes) the majority of the Namespace attributes of Resolver (wrappers and types) followed the plural convention and only algo followed the singular convention.
    ** This consequently led me to rename “metagraph.algo” -> “metagraph.algos”
    * It wasn’t clear how to change use the entry point declarations in setup.py to have a plugin_name attribute be associated with each plugin (outside of having a separate entry point for the name and one for the plugin, which didn’t seem ideal since it looks like each entry point should be processed separately).
    * I changed the PluginRegistry attributes to separate the concrete algorithms and types by name.
    * I made load_plugins return a PluginRegistry instead of a set of plugins.
    ** It seemed consistent with the comment “# Use this as the entry_point object” in metagraph/plugins/__init__.py .
    ** It seemed nicer to return a single object whose attributes separated the concrete algorithms and types by plugin name.
    * I made Resolver.register take in a PluginRegistry instead of sets of abstract types, wrappers, etc. since the PluginRegistry now separates translators, concrete types, etc. by plugin name.
    * I deconflated PluginRegistry.register into PluginRegistry.register_abstract and PluginRegistry.register_concrete .
    * Because Resolver.register now takes a PluginRegistry instance, Resolver.register no longer errors when we see repeated abstract types since two different PluginRegistry instances (for two different plugins) might implement the same abstract types. Since we want to use resolver.plugin.<plugin_label> with same tree structure as resolver (e.g. be able to call resolver.plugin.my_plugin.some_abstract_type_supported_in_my_plugin), the PluginRegistry will have to contain previously-seen-by-the-resolver abstract types since we might call Resolver.register them at different times with different registries.

--- metagraph/core/entrypoints.py
+++ metagraph/core/entrypoints.py
@@ -11,46 +11,32 @@ setup(
     },
 )
 
-The find_plugins function should return a dict of lists of the appropriate classes
-(types) or objects (translators and functions).
-Allowable keys in the plugins dict:
- - abstract_types
- - concrete_types
- - wrappers
- - translators
- - abstract_algorithms
- - concrete_algorithms
+The find_plugins function should return a metagraph.PluginRegistry of the relevant plugins.
 """
 
 from typing import Set, Dict, Callable
 import importlib_metadata
+from .plugin_registry import PluginRegistry
 
 
 class EntryPointsError(Exception):
     pass
 
 
-def load_plugins() -> Dict[str, Set]:
-    """Return a list of plugins of particular kind.
-
-    See find_plugin_loaders() for valid kind values.
-    """
-    plugin_loaders = importlib_metadata.entry_points().get("metagraph.plugins", [])
-    plugins = {}
+def load_plugins() -> PluginRegistry:
+    entry_points = importlib_metadata.entry_points().get("metagraph.plugins", [])
+    combined_registry = PluginRegistry()
     seen = set()
-    for pl in plugin_loaders:
-        if pl.name != "plugins":
+    for entry_point in entry_points:
+        if entry_point.name != "plugins":
             raise EntryPointsError(
-                f"metagraph.plugin found an unexpected entry_point: {pl.name}"
+                f"metagraph.plugin found an unexpected entry_point: {entry_point.name}"
             )
-        elif pl not in seen:
-            plugin_loader = pl.load()
+        elif entry_point not in seen:
+            plugin_loader = entry_point.load()
 
-            plugin_items = plugin_loader()
-            for key, vals in plugin_items.items():
-                if key not in plugins:
-                    plugins[key] = set(vals)
-                else:
-                    plugins[key].update(vals)
-            seen.add(pl)
-    return plugins
+            current_registry = plugin_loader()
+            combined_registry.update(current_registry)
+            seen.add(entry_point)
+
+    return combined_registry
