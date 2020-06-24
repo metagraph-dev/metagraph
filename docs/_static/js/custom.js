4af5aeb4010b829304effaab3a3a1fe49d6d1dad
Paul Nguyen <pnguyen@0584.attlocal.net>
2020-06-24 14:57:28 -0500

    Complete first draft of Algorithm List and Visualization parts of the User Guide.
    This commit completes the first draft of the Algorithm List and Visualization sections of the User Guide.
    
    The algorithm list contains a description of all the abstract algorithms currently supported in the metagraph core plugins.
    
    The visualization section describes why visualization's difficult to fit within the metagraoh workflow and forwards readers to suggestion any ideas they might have.

--- docs/_static/js/custom.js
+++ docs/_static/js/custom.js
@@ -4,3 +4,19 @@ const toggleIconSVGAnimation = (cardElement) => {
 	svg.classList.toggle('custom-icon-animate');
     });
 };
+
+const makeCustomButtonsMatchChildLinks = () => {
+    Array.prototype.forEach.call(document.getElementsByClassName('custom-button'), customButton => {
+        const anchors = customButton.getElementsByTagName('a');
+        if (anchors.length != 1) {
+            console.warn(`${customButton} got an unexpected number of child anchors.`);
+        } else {
+            const anchor = anchors[0];
+            const href = anchor.getAttribute('href');
+            const wrappingAnchor = document.createElement('a');
+            wrappingAnchor.setAttribute('href', href);
+            customButton.parentNode.insertBefore(wrappingAnchor, customButton);
+            wrappingAnchor.appendChild(customButton);
+        }
+    });
+};
