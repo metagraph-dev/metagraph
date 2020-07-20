e8ce2e60feb18631ab0ec56fde4b175d70580b8c
Paul Nguyen <paul.tqh.nguyen@gmail.com>
2020-07-20 13:51:29 -0700

    Merge utility changes with NumpyNodeMap changes
    NumpyNodeMap was changed to have all the functionality of what was formerly known as CompactNumpyNodeMap.
    
    The NodeSet and NodeMap utility algorithms had to be changed to match the latest NumpyNodeMap implementation.
    
    This commit attempts to do so.

--- metagraph/plugins/numpy/algorithms.py
+++ metagraph/plugins/numpy/algorithms.py
@@ -1,25 +1,32 @@
 import numpy as np
 from functools import reduce
 from metagraph import concrete_algorithm, NodeID
-from .types import NumpyVector, NumpyNodeMap, CompactNumpyNodeMap, NumpyNodeSet
+from .types import NumpyVector, NumpyNodeMap, NumpyNodeSet
 from typing import Any, Callable, Optional
 
 
 @concrete_algorithm("util.nodeset.choose_random")
 def np_nodeset_choose_random(x: NumpyNodeSet, k: int) -> NumpyNodeSet:
-    return NumpyNodeSet(np.random.choice(np.array(x.value), k, False))
+    random_elements = np.random.choice(np.array(x.value), k, False)
+    random_elements.sort()
+    return NumpyNodeSet(random_elements)
 
 
 @concrete_algorithm("util.nodemap.sort")
 def np_nodemap_sort(
     x: NumpyNodeMap, ascending: bool, limit: Optional[int]
 ) -> NumpyVector:
-    ids_of_sorted_values = np.argsort(x.value)
-    if x.missing_mask is not None:
-        missing_value_ids = np.argwhere(x.missing_mask).flatten()
+    if x.id2pos is not None:
+        positions_of_sorted_values = np.argsort(x.value)
+        ids_of_sorted_values = x.pos2id[positions_of_sorted_values]
+    elif x.mask is not None:
+        ids_of_sorted_values = np.argsort(x.value)
+        missing_value_ids = np.flatnonzero(~x.mask)
         ids_of_sorted_values = np.setdiff1d(
             ids_of_sorted_values, missing_value_ids, assume_unique=True
         )
+    else:
+        ids_of_sorted_values = np.argsort(x.value)
     if not ascending:
         ids_of_sorted_values = np.flip(ids_of_sorted_values)
     if limit:
@@ -27,83 +34,65 @@ def np_nodemap_sort(
     return NumpyVector(ids_of_sorted_values)
 
 
-# @concrete_algorithm("util.nodemap.sort")
-# def np_compact_nodemap_sort(x: CompactNumpyNodeMap, ascending: bool, limit: Optional[int]) -> NumpyVector:
-#     indices_of_sorted_values = np.argsort(x.value)
-#     if limit:
-#         indices_of_sorted_values = indices_of_sorted_values[:limit]
-#     reverse_lookup = {value: key for key, value in x.lookup} # TODO consider caching this
-#     ids_of_sorted_values = np.array([reverse_lookup[index] for index in indices_of_sorted_values])
-#     if not ascending:
-#         ids_of_sorted_values = np.flip(ids_of_sorted_values)
-#     return NumpyVector(indices_of_sorted_values)
-
-
 @concrete_algorithm("util.nodemap.select")
 def np_nodemap_select(x: NumpyNodeMap, nodes: NumpyNodeSet) -> NumpyNodeMap:
-    selected_node_map = NumpyNodeMap(x.value.copy(), missing_mask=x.missing_mask.copy())
-    present_value_indices = np.argwhere(~selected_node_map.missing_mask).flatten()
-    indices_to_remove = np.setdiff1d(present_value_indices, nodes.value)
-    selected_node_map.missing_mask[indices_to_remove] = True
+    if x.id2pos is not None:
+        new_pos2id = np.intersect1d(x.pos2id, nodes.value)
+        positions_to_keep = np.array([x.id2pos[node_id] for node_id in new_pos2id])
+        new_data = x.value[positions_to_keep].copy()
+        selected_node_map = NumpyNodeMap(new_data, node_ids=new_pos2id)
+    else:
+        nodes.value.sort()
+        selected_node_map = NumpyNodeMap(
+            x.value.copy(),
+            mask=x.mask.copy() if x.mask else np.ones(len(x.value), dtype=bool),
+        )
+        present_value_positions = np.flatnonzero(selected_node_map.mask)
+        positions_to_remove = np.setdiff1d(
+            present_value_positions, nodes.value, assume_unique=True
+        )
+        selected_node_map.mask[positions_to_remove] = False
     return selected_node_map
-
-
-# @concrete_algorithm("util.nodemap.select")
-# def np_compact_nodemap_select(x: CompactNumpyNodeMap, nodes: NumpyNodeSet) -> CompactNumpyNodeMap:
-#     new_lookup = {node_id: node_index for node_id, node_index in x.lookup.items() if np.isin(node_id, nodes.value)}
-#     retained_indices = np.array(list(new_lookup.values()))
-#     new_data = x.value[retained_indices]
-#     old_index_to_new_index = retained_indices
-#     new_lookup = {node_id: old_index_to_new_index[old_node_index] for node_id, old_node_index in new_lookup.items()}
-#     return NumpyNodeMap(new_data, node_lookup=new_lookup)
 
 
 @concrete_algorithm("util.nodemap.filter")
 def np_nodemap_filter(x: NumpyNodeMap, func: Callable[[Any], bool]) -> NumpyNodeSet:
-    present_value_indices = np.argwhere(~x.missing_mask).flatten()
-    present_values = x.value[present_value_indices]
-    func_vectorized = np.vectorize(
-        func
-    )  # TODO consider caching this somewhere or enforcing that only vectorized functions are given
-    filtered_indices = present_value_indices[func_vectorized(present_values)]
-    return NumpyNodeSet(filtered_indices)
-
-
-# @concrete_algorithm("util.nodemap.filter")
-# def np_compact_nodemap_filter(x: CompactNumpyNodeMap, func: Callable[[Any], bool]) -> NumpyNodeSet:
-#     func_vectorized = np.vectorize(func) # TODO consider caching this somewhere or enforcing that only vectorized functions are given
-#     retained_indices = np.argwhere(func_vectorized(x.value)).flatten()
-#     new_data = x.value[retained_indices]
-#     old_index_to_new_index = retained_indices
-#     new_lookup = {node_id: old_index_to_new_index[old_node_index] for node_id, old_node_index in x.lookup.items() if old_node_index in retained_indices}
-#     return NumpyNodeMap(new_data, node_lookup=new_lookup)
+    # TODO consider caching this somewhere or enforcing that only vectorized functions are given
+    func_vectorized = np.vectorize(func)
+    if x.id2pos is not None:
+        filtered_positions = np.flatnonzero(func_vectorized(x.value))
+        filtered_ids = x.pos2id[filtered_positions]
+    elif x.mask is not None:
+        present_values_filter_applied = func_vectorized(x.value[x.mask])
+        filter_mask = np.zeros(len(x.value), dtype=bool)
+        filter_mask[x.mask] = present_values_filter_applied
+        filtered_ids = np.flatnonzero(filter_mask)
+    else:
+        filtered_ids = np.flatnonzero(func_vectorized(x.value))
+    return NumpyNodeSet(filtered_ids)
 
 
 @concrete_algorithm("util.nodemap.apply")
 def np_nodemap_apply(x: NumpyNodeMap, func: Callable[[Any], Any]) -> NumpyNodeMap:
-    func_vectorized = np.vectorize(
-        func
-    )  # TODO consider caching this somewhere or enforcing that only vectorized functions are given
-    return NumpyNodeMap(func_vectorized(x.value), missing_mask=x.missing_mask.copy())
-
-
-# @concrete_algorithm("util.nodemap.apply")
-# def np_compact_nodemap_apply(x: CompactNumpyNodeMap, func: Callable[[Any], Any]) -> CompactNumpyNodeMap:
-#     func_vectorized = np.vectorize(func) # TODO consider caching this somewhere or enforcing that only vectorized functions are given
-#     return CompactNumpyNodeMap(func_vectorized(x.value), node_lookup=x.node_lookup) # TODO copy node_lookup or not?
+    # TODO consider caching this somewhere or enforcing that only vectorized functions are given
+    func_vectorized = np.vectorize(func)
+    if x.id2pos is not None:
+        new_node_map = NumpyNodeMap(func_vectorized(x.value), node_ids=x.pos2id.copy())
+    elif x.mask is not None:
+        new_node_map = NumpyNodeMap(x.value.copy(), missing_mask=x.missing_mask.copy())
+        new_node_map.value[new_node_map.mask] = func_vectorized(
+            new_node_map.value[new_node_map.mask]
+        )
+    else:
+        new_node_map = NumpyNodeMap(func_vectorized(x.value))
+    return new_node_map
 
 
 @concrete_algorithm("util.nodemap.reduce")
 def np_nodemap_reduce(x: NumpyNodeMap, func: Callable[[Any, Any], Any]) -> Any:
-    present_values = x.value[~x.missing_mask]
+    present_values = x.value if x.mask is None else x.value[x.mask]
     return (
         func.reduce(present_values)
         if isinstance(func, np.ufunc)
         else reduce(func, present_values)
     )
-
-
-# @concrete_algorithm("util.nodemap.reduce")
-# def np_compact_nodemap_reduce(x: NumpyNodeMap, func: Callable[[Any, Any], Any]) -> Any:
-#     present_values = x.value
-#     return func.reduce(present_values) if isinstance(func, np.ufunc) else reduce(func, present_values)
--- metagraph/tests/translators/test_node_set.py
+++ metagraph/tests/translators/test_node_set.py
@@ -1,11 +1,7 @@
 import pytest
 from metagraph.tests.util import default_plugin_resolver
 from metagraph.plugins.python.types import PythonNodeSet
-from metagraph.plugins.numpy.types import (
-    NumpyNodeSet,
-    NumpyNodeMap,
-    CompactNumpyNodeMap,
-)
+from metagraph.plugins.numpy.types import NumpyNodeSet, NumpyNodeMap
 import numpy as np
 
 
@@ -13,23 +9,14 @@ def test_np_nodemap_2_np_nodeset(default_plugin_resolver):
     dpr = default_plugin_resolver
     x = NumpyNodeMap(np.array([00, 10, 20]))
     assert x.num_nodes == 3
-    intermediate = NumpyNodeSet(np.array([20, 00, 10]))
-    y = dpr.translate(x, NumpyNodeSet)
-    dpr.assert_equal(y, intermediate)
-
-
-def test_np_compact_nodemap_2_np_nodeset(default_plugin_resolver):
-    dpr = default_plugin_resolver
-    x = CompactNumpyNodeMap(np.array([11.1, 33.3, -22.2]), {0: 2, 1: 1, 2: 0},)
-    assert x.num_nodes == 3
-    intermediate = NumpyNodeSet(np.array([-22.2, 11.1, 33.3]))
+    intermediate = NumpyNodeSet(np.array([0, 1, 2]))
     y = dpr.translate(x, NumpyNodeSet)
     dpr.assert_equal(y, intermediate)
 
 
 def test_np_nodeset_2_py_nodeset(default_plugin_resolver):
     dpr = default_plugin_resolver
-    x = NumpyNodeSet(np.array([9, 5, 1]))
+    x = NumpyNodeSet(np.array([1, 5, 9]))
     assert x.num_nodes == 3
     intermediate = PythonNodeSet({5, 1, 9})
     y = dpr.translate(x, PythonNodeSet)
@@ -40,6 +27,6 @@ def test_py_nodeset_2_np_nodeset(default_plugin_resolver):
     dpr = default_plugin_resolver
     x = PythonNodeSet({5, 1, 9})
     assert x.num_nodes == 3
-    intermediate = NumpyNodeSet(np.array([9, 5, 1]))
+    intermediate = NumpyNodeSet(np.array([1, 5, 9]))
     y = dpr.translate(x, NumpyNodeSet)
     dpr.assert_equal(y, intermediate)
