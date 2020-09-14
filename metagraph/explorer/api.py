ec6bebb442e1aec6d91de474e7f4eafeb739fac7
Paul Nguyen <pnguyen@0584.attlocal.net>
2020-09-14 15:17:27 -0500

    Plugin Explorer: Display all plugin parts while de-emphasizing the empty ones
    The plugin explorer used to only show the plugin parts available in a plugin.
    
    It now shows all of them, but the plugin parts that are empty are shown in grey.

--- metagraph/explorer/api.py
+++ metagraph/explorer/api.py
@@ -57,58 +57,62 @@ def get_plugins(resolver, **kwargs):
         result[plugin_name] = OrderedDict()
         plugin = getattr(plugins, plugin_name)
         result[plugin_name]["children"] = OrderedDict()
-        for plugin_part_name in sorted(dir(plugin)):
+        plugin_part_names = sorted(dir(plugin))
+        get_pretty_plugin_part_name = lambda plugin_part_name: " ".join(
+            map(str.capitalize, plugin_part_name.split("_"))
+        )
+        for plugin_part_name in plugin_part_names:
             plugin_part = getattr(plugin, plugin_part_name)
+            pretty_plugin_part_name = get_pretty_plugin_part_name(plugin_part_name)
             if plugin_part_name == "abstract_algorithms":
-                if len(plugin_part) > 0:
-                    result[plugin_name]["children"]["Abstract Algorithms"] = {
-                        "children": OrderedDict()
-                    }
-                    for abstract_algorithm_name in sorted(plugin_part.keys()):
-                        result[plugin_name]["children"]["Abstract Algorithms"][
-                            "children"
-                        ][
-                            abstract_algorithm_name
-                        ] = {}  # TODO add other useful information here
+
+                result[plugin_name]["children"][pretty_plugin_part_name] = {
+                    "children": OrderedDict()
+                }
+                for abstract_algorithm_name in sorted(plugin_part.keys()):
+                    result[plugin_name]["children"][pretty_plugin_part_name][
+                        "children"
+                    ][
+                        abstract_algorithm_name
+                    ] = {}  # TODO add other useful information here
             elif plugin_part_name == "abstract_types":
                 abstract_type_names = sorted(map(get_name, plugin_part))
-                if len(abstract_type_names) > 0:
-                    result[plugin_name]["children"]["Abstract Types"] = {
-                        "children": OrderedDict()
-                    }
-                    for abstract_type_name in abstract_type_names:
-                        result[plugin_name]["children"]["Abstract Types"]["children"][
-                            abstract_type_name
-                        ] = {}  # TODO add other useful information here
+                result[plugin_name]["children"][pretty_plugin_part_name] = {
+                    "children": OrderedDict()
+                }
+                for abstract_type_name in abstract_type_names:
+                    result[plugin_name]["children"][pretty_plugin_part_name][
+                        "children"
+                    ][
+                        abstract_type_name
+                    ] = {}  # TODO add other useful information here
             elif plugin_part_name == "concrete_algorithms":
-                if len(plugin_part) > 0:
-                    result[plugin_name]["children"]["Concrete Algorithms"] = {
-                        "children": OrderedDict()
-                    }
-                    for abstract_algorithm_name in sorted(plugin_part.keys()):
-                        concrete_algorithms = sorted(
-                            plugin_part[abstract_algorithm_name]
-                        )
-                        result[plugin_name]["children"]["Concrete Algorithms"][
+                result[plugin_name]["children"][pretty_plugin_part_name] = {
+                    "children": OrderedDict()
+                }
+                for abstract_algorithm_name in sorted(plugin_part.keys()):
+                    concrete_algorithms = sorted(plugin_part[abstract_algorithm_name])
+                    result[plugin_name]["children"][pretty_plugin_part_name][
+                        "children"
+                    ][abstract_algorithm_name] = {"children": OrderedDict()}
+                    for concrete_algorithm in concrete_algorithms:
+                        concrete_algorithm_name = concrete_algorithm.__name__
+                        result[plugin_name]["children"][pretty_plugin_part_name][
                             "children"
-                        ][abstract_algorithm_name] = {"children": OrderedDict()}
-                        for concrete_algorithm in concrete_algorithms:
-                            concrete_algorithm_name = concrete_algorithm.__name__
-                            result[plugin_name]["children"]["Concrete Algorithms"][
-                                "children"
-                            ][abstract_algorithm_name]["children"][
-                                concrete_algorithm_name
-                            ] = {}  # TODO add other useful information here
+                        ][abstract_algorithm_name]["children"][
+                            concrete_algorithm_name
+                        ] = {}  # TODO add other useful information here
             elif plugin_part_name == "concrete_types":
                 concrete_types = sorted(map(get_name, plugin_part))
-                if len(concrete_types) > 0:
-                    result[plugin_name]["children"]["Concrete Types"] = {
-                        "children": OrderedDict()
-                    }
-                    for concrete_type in concrete_types:
-                        result[plugin_name]["children"]["Concrete Types"]["children"][
-                            concrete_type
-                        ] = {}  # TODO add other useful information here
+                result[plugin_name]["children"][pretty_plugin_part_name] = {
+                    "children": OrderedDict()
+                }
+                for concrete_type in concrete_types:
+                    result[plugin_name]["children"][pretty_plugin_part_name][
+                        "children"
+                    ][
+                        concrete_type
+                    ] = {}  # TODO add other useful information here
             elif plugin_part_name == "translators":
                 translator_strings = sorted(
                     [
@@ -116,28 +120,28 @@ def get_plugins(resolver, **kwargs):
                         for src, dst in plugin_part.keys()
                     ]
                 )
-                if len(translator_strings) > 0:
-                    result[plugin_name]["children"]["Translators"] = {
-                        "children": OrderedDict()
-                    }
-                    for translator_string in translator_strings:
-                        result[plugin_name]["children"]["Translators"]["children"][
-                            translator_string
-                        ] = {}  # TODO add other useful information here
+                result[plugin_name]["children"][pretty_plugin_part_name] = {
+                    "children": OrderedDict()
+                }
+                for translator_string in translator_strings:
+                    result[plugin_name]["children"][pretty_plugin_part_name][
+                        "children"
+                    ][
+                        translator_string
+                    ] = {}  # TODO add other useful information here
             elif plugin_part_name == "wrappers":
                 abstract_type_names = sorted(dir(plugin_part))
-                if len(abstract_type_names) > 0:
-                    result[plugin_name]["children"]["Wrappers"] = {
-                        "children": OrderedDict()
-                    }
-                    for abstract_type_name in abstract_type_names:
-                        abstract_type_namespace = getattr(
-                            plugin_part, abstract_type_name
-                        )
-                        for concrete_type_name in sorted(dir(abstract_type_namespace)):
-                            result[plugin_name]["children"]["Wrappers"]["children"][
-                                concrete_type_name
-                            ] = {}  # TODO add other useful information here
+                result[plugin_name]["children"][pretty_plugin_part_name] = {
+                    "children": OrderedDict()
+                }
+                for abstract_type_name in abstract_type_names:
+                    abstract_type_namespace = getattr(plugin_part, abstract_type_name)
+                    for concrete_type_name in sorted(dir(abstract_type_namespace)):
+                        result[plugin_name]["children"][pretty_plugin_part_name][
+                            "children"
+                        ][
+                            concrete_type_name
+                        ] = {}  # TODO add other useful information here
             else:
                 pass  # TODO consider showing algorithm versions
     return result
