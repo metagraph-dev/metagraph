c2960b966734780725c392c4f2317b7d4f8eccde
Erik Welch <erik.n.welch@gmail.com>
2021-10-13 12:33:32 -0500

    Update comparison of `grblas` version

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ setup(
         "nest-asyncio",
         "dask[array,dataframe,delayed]",
         "graphviz",
+        "packaging",
     ],
     include_package_data=True,
     entry_points={"metagraph.plugins": ["plugins=metagraph.plugins:find_plugins",]},
